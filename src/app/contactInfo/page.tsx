d4ec7683a952ef05eda814a1c0b78783c53a33e3
vineela666 <veerankivineela14@gmail.com>
2025-11-03 10:36:32 +0530

    Added Footer component to home page

--- src/app/contactInfo/page.tsx
+++ src/app/contactInfo/page.tsx
@@ -1,6 +1,6 @@
 export default function ContactInfo() {
     return <>
-        <div>
+        <div className="flex flex-col gap-4 w-100   bg-cyan-500">
             <h1>Contact Info</h1>
             <input type="text"  placeholder="Full name"/><br/>
             <input type="email"  placeholder="E-mail"/><br/>
